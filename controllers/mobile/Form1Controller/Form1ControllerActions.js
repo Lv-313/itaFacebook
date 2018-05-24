4800959e0dcfdf9ddcdc3a0586ef96c3b72e328d
Ivan Pykalo <ipykaloi@gmail.com>
2018-05-24 10:35:57 +0300

    save

--- controllers/mobile/Form1Controller/Form1ControllerActions.js
+++ controllers/mobile/Form1Controller/Form1ControllerActions.js
@@ -6,5 +6,9 @@ define({
     AS_Button_g9f6ddd2e9e547be8813342cb33fea6e: function AS_Button_g9f6ddd2e9e547be8813342cb33fea6e(eventobject) {
         var self = this;
         return self.login.call(this);
+    },
+    /** onClick defined for bodyContainer **/
+    AS_FlexContainer_b43b867e295741d2bb88b76add8aded7: function AS_FlexContainer_b43b867e295741d2bb88b76add8aded7(eventobject) {
+        var self = this;
     }
 });
